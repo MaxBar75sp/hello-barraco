4a4ecd01f9e3edafdee5c04751d4e53aa4a3ec94
MaxBar75sp <themaxbar75sp@gmail.com>
2025-04-24 07:34:10 +0000

    Commit secondario nome 3 volte

--- hello-barraco/Hello.java
+++ hello-barraco/Hello.java
@@ -1,9 +1,17 @@
 import java.util.Scanner;
+
 public class Hello {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
-        System.out.println("Hello, " + name + "!");
+        String name = scanner.nextLine();   
+    //Richiamo la funzione printName che stampa il nome 3 volte
+    printName(name);
+    }
+    //Funzione che stampa il nome 3 volte:
+    public static void printName(String name) {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Hello, " + name + "!");
+        }
     }
 }   
