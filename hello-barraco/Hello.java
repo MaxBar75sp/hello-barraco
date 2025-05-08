63a5893cb3e90bbb105a86eb8ed4edb175885efa
MaxBar75sp <themaxbar75sp@gmail.com>
2025-05-08 09:03:43 +0200

    Update Hello.java

--- hello-barraco/Hello.java
+++ hello-barraco/Hello.java
@@ -15,20 +15,6 @@ public class Hello {
         }
         System.out.println(); // A capo dopo la stampa
 
-        // Parte aggiunta: stampa "Hello Fabio!" con Vector
-        System.out.println("Hello Marco!");
-
-        Vector<Character> lettere = new Vector<>();
-        lettere.add('m');
-        lettere.add('a');
-        lettere.add('r');
-        lettere.add('c');
-        lettere.add('o');
-
-        for (Character lettera : lettere) {
-            System.out.println(lettera);
-        }
-
         // Scanner per input utente
         Scanner scanner = new Scanner(System.in);
 
